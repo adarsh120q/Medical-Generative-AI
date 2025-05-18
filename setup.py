371031235562715427f3d4352291c715d3caf29a
Adarsh Singh <adarshsingh120q@gmail.com>
2025-05-18 12:35:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Genarative AI Project',
+    version= '0.0.0',
+    author= 'Adarsh Singh',
+    author_email= 'adarshsingh120q@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
